9ac39d0ae53b274937a02fa99640b95372c54097
Kat <kat.humphries.dev@gmail.com>
2022-07-16 22:31:22 -0400

    client side rendering

---
+++ pages/pokemon/[id].js
@@ -0,0 +1,96 @@
+/* eslint-disable @next/next/no-img-element */
+import Head from 'next/head';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import React, { useEffect, useState } from 'react';
+import styles from '../../styles/Details.module.css';
+
+// export async function getStaticPaths() {
+//   const resp = await fetch('https://jherr-pokemon.s3.us-west-1.amazonaws.com/index.json');
+//   const pokemon = await resp.json();
+
+//   return {
+//     paths: pokemon.map(pokemon => ({
+//       params: { id: pokemon.id.toString() }
+//     })),
+//     fallback: false
+//   };
+// }
+
+// export async function getStaticProps({ params }) {
+//   const resp = await fetch(`https://jherr-pokemon.s3.us-west-1.amazonaws.com/pokemon/${params.id}.json`);
+
+//   return {
+//     props: {
+//       pokemon: await resp.json()
+//     }
+//     // revalidate: 30,
+//   };
+// }
+
+export default function Details() {
+  const {
+    query: { id }
+  } = useRouter();
+
+  const [pokemon, setPokemon] = useState(null);
+
+  useEffect(() => {
+    async function getPokemon() {
+      const response = await fetch(`https://jherr-pokemon.s3.us-west-1.amazonaws.com/pokemon/${2}.json`);
+      // const response = await fetch('https://pokeapi.co/api/v2/pokemon/clefairy/');
+      setPokemon(await response.json());
+    }
+
+    if (id) {
+      getPokemon();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [id]);
+
+  if (!pokemon) {
+    return null;
+  }
+
+  return (
+    <div>
+      <Head>
+        <title>{pokemon.name}</title>
+      </Head>
+      <div>
+        <Link href='/'>
+          <a>Back to Home</a>
+        </Link>
+      </div>
+      <div className={styles.layout}>
+        <div>
+          <img
+            className={styles.picture}
+            src={`https://jherr-pokemon.s3.us-west-1.amazonaws.com/${pokemon.image}`}
+            alt={pokemon.name.english}
+          />
+        </div>
+        <div>
+          <div className={styles.name}>{pokemon.name}</div>
+          <div className={styles.type}>{pokemon.type.join(', ')}</div>
+          <table>
+            <thead className={styles.header}>
+              <tr>
+                <th>Name</th>
+                <th>Value</th>
+              </tr>
+            </thead>
+            <tbody>
+              {pokemon.stats.map(({ name, value }) => (
+                <tr key={name}>
+                  <td className={styles.attribute}>{name}</td>
+                  <td>{value}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  );
+}
